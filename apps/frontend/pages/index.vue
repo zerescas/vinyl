ef0d3f6cc2e1c78baaf2dab6b4ef95f36fc8ab4c
Alexander Zakurnaev <zerescas@gmail.com>
2024-10-01 18:40:50 +0900

    feat(frontend): create pages "/", "/test"

---
+++ apps/frontend/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>
+      Welcome,
+      <span class="font-bold text-[--color-accent-950] drop-shadow-accent-md">
+        zerescas</span
+      >
+    </h1>
+    <p>Has the time come?</p>
+  </div>
+</template>
---
+++ apps/frontend/pages/test/index.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Test</h1>
+  <p>Nothing here</p>
+</template>
