925648cd6dcc2e06d0b8ac2cc7917bb443c77c01
Alexander Zakurnaev <zerescas@gmail.com>
2024-10-01 22:14:54 +0900

    feat(frontend): implement AppHeader component

---
+++ apps/frontend/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import IcRoundMenu from "~icons/ic/round-menu";
+import IcRoundClose from "~icons/ic/round-close";
+
+const sidebarStore = useSidebarStore();
+const { isExpanded } = storeToRefs(sidebarStore);
+</script>
+
+<template>
+  <div class="flex items-center px-5 py-5">
+    <!-- Expand menu -->
+    <button
+      class="z-10 translate-y-0.5 text-zinc-50 transition-colors hover:text-zinc-300"
+      @click="isExpanded = !isExpanded"
+    >
+      <IcRoundMenu v-show="!isExpanded" class="size-7" />
+      <IcRoundClose v-show="isExpanded" class="size-7" />
+    </button>
+
+    <!-- App Title -->
+    <RouterLink
+      class="z-10 ml-4 text-2xl tracking-wide transition-all md:ml-12 [&.expanded]:ml-4"
+      :class="{ expanded: isExpanded }"
+      to="/"
+    >
+      <span class="font-bold text-[--color-accent-950] drop-shadow-accent"
+        >V</span
+      >
+      <span>inyl</span>
+    </RouterLink>
+  </div>
+</template>
