8d63bb947f243a44ed80c7b9de4dc04523d68dcb
Alexander Zakurnaev <zerescas@gmail.com>
2024-10-01 17:43:25 +0900

    chore(frontend): update tailwind config, add specific drop shadows with color accent

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -5,6 +5,12 @@ export default {
   },
   theme: {
     extend: {
+      dropShadow: {
+        "accent-sm": "0 0 4px var(--color-accent-950)",
+        accent: "0 0 8px var(--color-accent-950)",
+        "accent-md": "0 0 16px var(--color-accent-950)",
+      },
+
       transitionProperty: {
         size: "height, width",
         ["max-size"]: "max-height, max-width",
