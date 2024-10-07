70f5b662a251a79e7c2fe39ddfdce98848a0fe81
Alexander Zakurnaev <zerescas@gmail.com>
2024-10-07 11:25:10 +0900

    feat(frontend): create pages for sign-up, sign-in

---
+++ apps/frontend/pages/auth/sign-in/index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const username = ref("");
+const password = ref("");
+
+const { signIn } = useAuth();
+</script>
+
+<template>
+  <h1>Sign In</h1>
+  <p>Welcome back</p>
+
+  <form
+    class="mt-8 flex flex-col gap-6"
+    @submit.prevent="signIn(username, password)"
+  >
+    <AppInputGroup>
+      <AppInput label="Username" v-model="username" />
+      <AppInput label="Password" type="password" v-model="password" />
+    </AppInputGroup>
+
+    <button
+      class="mt-4 self-start rounded-md bg-zinc-200 px-8 py-1.5 font-semibold text-zinc-950 hover:bg-zinc-100"
+      type="submit"
+    >
+      Sign In
+    </button>
+  </form>
+</template>
---
+++ apps/frontend/pages/auth/sign-up/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Sign Up</h1>
+
+  <form class="mt-8 flex flex-col gap-6">
+    <AppInputGroup>
+      <AppInput label="Username" />
+      <AppInput label="Email" />
+      <AppInput label="Password" type="password" />
+    </AppInputGroup>
+
+    <button
+      class="mt-4 self-start rounded-md bg-zinc-200 px-8 py-1.5 font-semibold text-zinc-950 hover:bg-zinc-100"
+      type="submit"
+    >
+      Sign Up
+    </button>
+  </form>
+</template>
