8ecfbdb54233fcac777ed565b8528f6b585ffc72
Alexander Zakurnaev <zerescas@gmail.com>
2024-10-07 10:54:44 +0900

    feat(backend): update main.ts | setup CORS, use cookieParser, check "JWT_SECRET" env

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from "@nestjs/core";
+import * as cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+function checkEnvironmentVariables() {
+  if (!process.env.JWT_SECRET) throw new Error(`"JWT_SECRET" is empty!`);
+}
 
+async function bootstrap() {
+  checkEnvironmentVariables();
+
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
+  app.use(cookieParser());
+  app.enableCors({ origin: ["http://localhost:3000"], credentials: true });
 
   await app.listen(9000);
 }
