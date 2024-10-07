5c13e52577c5ae33d153cbfb49073dac390e0b0b
Alexander Zakurnaev <zerescas@gmail.com>
2024-10-07 11:26:48 +0900

    feat(frontend): update /, improve welcome message Improved welcome message:   - Use "stranger" for unauthorized users, use username for authorized users   - Added animated skeleton to user name on page loading

--- apps/frontend/pages/index.vue
+++ apps/frontend/pages/index.vue
@@ -1,13 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const userStore = useUserStore();
+</script>
 
 <template>
   <div>
-    <h1>
+    <h1 class="">Home</h1>
+
+    <p class="flex gap-2">
       Welcome,
-      <span class="font-bold text-[--color-accent-950] drop-shadow-accent-md">
-        zerescas</span
-      >
-    </h1>
-    <p>Has the time come?</p>
+      <ClientOnly>
+        <span class="font-bold text-[--color-accent-950] drop-shadow-accent-md">
+          {{ userStore.user.username || "Stranger" }}
+        </span>
+
+        <template #fallback>
+          <span
+            class="w-36 animate-pulse rounded-xl bg-zinc-700 align-middle"
+          ></span>
+        </template>
+      </ClientOnly>
+    </p>
   </div>
 </template>
