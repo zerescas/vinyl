7abdd9c0616ee48c5cbe8bdd87b13ea9f042f6a7
Alexander Zakurnaev <zerescas@gmail.com>
2024-10-08 13:42:25 +0900

    feat(frontend): create unified components for usage in /auth/*

---
+++ apps/frontend/components/AuthForm.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="sm:max-w-80">
+    <div class="flex h-16 flex-col justify-around">
+      <slot name="headerContent" />
+    </div>
+
+    <form class="mt-8 flex flex-col gap-6">
+      <slot name="formContent" />
+    </form>
+  </div>
+</template>
---
+++ apps/frontend/components/AuthFormButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="mt-4 self-start rounded-md bg-zinc-200 px-8 py-1.5 font-semibold text-zinc-950 hover:bg-zinc-100"
+  >
+    <slot />
+  </button>
+</template>
